b4a9cd933238c65c4a1860670f0f44239777bcf2
lmspizzirri <lm.spizzirri@gmail.com>
2023-01-15 00:00:20 -0500

    Req 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,42 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    disabled: true,
+  };
+
+  onInputChange = ({ target }) => {
+    const { value } = target;
+    const minLength = 2;
+    if (value.length >= minLength) {
+      this.setState({
+        disabled: false,
+      });
+    } else {
+      this.setState({
+        disabled: true,
+      });
+    }
+  };
+
   render() {
+    const { disabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         <h1>Search</h1>
+        <input
+          type="text"
+          data-testid="search-artist-input"
+          onChange={ this.onInputChange }
+        />
+        <button
+          type="submit"
+          data-testid="search-artist-button"
+          disabled={ disabled }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
